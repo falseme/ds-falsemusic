3f31664e4d684b93bbd760c0f88c087f10f51d20
Fabricio <fabritomas25@gmail.com>
2023-09-14 19:23:07 -0300

    slf4j libraries

--- src/main/java/net/falseme/discord/falsemusic/bot/Main.java
+++ src/main/java/net/falseme/discord/falsemusic/bot/Main.java
@@ -2,6 +2,7 @@ package net.falseme.discord.falsemusic.bot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.falseme.discord.falsemusic.bot.command.MusicCommands;
 import net.falseme.discord.falsemusic.bot.event.CommandListener;
 import net.falseme.discord.falsemusic.bot.event.EventListeners;
@@ -23,6 +24,8 @@ public class Main {
 		Env.load();
 
 		JDA jdabot = JDABuilder.createDefault(Env.get("DISCORD_TOKEN")).build();
+
+		jdabot.getPresence().setActivity(Activity.listening("10 hours of nothing on youtube"));
 
 		jdabot.addEventListener(new EventListeners());
 		jdabot.addEventListener(new CommandListener(MusicCommands.loadCommands()));
